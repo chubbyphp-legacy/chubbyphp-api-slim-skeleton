7b2ae71665936f9e6801cb584650adef674a820c
Dominik Zogg <dominik.zogg@gmail.com>
2017-09-07 22:24:30 +0200

    use ip instead of localhost to be usable event without socket support

--- config/config.php
+++ config/config.php
@@ -30,7 +30,7 @@ return [
         ],
         'db.options' => [
             'driver' => 'pdo_mysql',
-            'host' => 'localhost',
+            'host' => '127.0.0.1',
             'port' => 3306,
             'dbname' => 'chubbyphp_api_skeleton',
             'user' => 'root',
